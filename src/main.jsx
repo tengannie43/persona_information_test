67b882f0ef3392da94ddb291c63df97563413c7b
tengannie43 <tengannie@gmail.com>
2025-06-29 22:27:46 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import LoginPage from './LoginPage'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './LoginPage';
+import PersonaInformationPage from './PersonaInformationPage';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LoginPage />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        {/* 預設首頁：登入頁 */}
+        <Route path="/" element={<LoginPage />} />
+        {/* 基本資料頁 */}
+        <Route path="/persona" element={<PersonaInformationPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
