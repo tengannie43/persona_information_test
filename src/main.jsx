55a4bb2cd849be13ecf42ffcbf7aa8206a4d4875
tengannie43 <tengannie@gmail.com>
2025-06-29 23:27:18 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import PersonaInformationPage from './PersonaInformationPage';
 import KidneyStatusPage from './KidneyStatusPage';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/persona" element={<PersonaInformationPage />} />
-        <Route path="/kidney" element={<KidneyStatusPage />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+<BrowserRouter>
+  <Routes>
+    {/* 設定首頁：登入頁 */}
+    <Route path="/" element={<LoginPage />} />
+    {/* 1-1 頁面 */}
+    <Route path="/persona" element={<PersonaInformationPage />} />
+    {/* 1-2 頁面 */}
+    <Route path="/kidney" element={<KidneyStatusPage />} />
+    {/* 預設跳轉，如果找不到路徑，自動回首頁 */}
+    <Route path="*" element={<Navigate to="/" />} />
+  </Routes>
+</BrowserRouter>
